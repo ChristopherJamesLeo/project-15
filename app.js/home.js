7e2b8c496343ef3f5326cc224cc294bf17926a3f
Christopher James Leo <111208339+ChristopherJamesLeo@users.noreply.github.com>
2022-11-12 18:47:28 +0630

    fixedNavBar

--- app.js/home.js
+++ app.js/home.js
@@ -1,5 +1,25 @@
 console.log("hello world");
 let headBar = document.querySelector(".main-menu");
+let navSlide = document.querySelector(".nav-slide-container");
+let dropDownBtns = document.querySelectorAll(".slide-nav-list-items div");
+let angles = document.querySelectorAll(".slide-nav-list-items div i")
+
 window.addEventListener("scroll",function(){
     headBar.classList.toggle("main-menu-slide" , window.scrollY > 0)
 })
+
+document.querySelector(".btn-container").addEventListener("click",function(){
+    navSlide.classList.toggle("nav-slide-container-show-hide");
+})
+document.querySelector(".close-btn i").addEventListener("click",()=>{
+    navSlide.classList.remove("nav-slide-container-show-hide");
+})
+
+angles.forEach(function(angle){
+    angle.addEventListener("click",function(){
+        this.classList.toggle("angle-rotate");
+    })
+})
+
+
+
