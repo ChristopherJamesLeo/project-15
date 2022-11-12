eb550d74ae6367a15d5cb6325da881119ce8ac36
Christopher James Leo <111208339+ChristopherJamesLeo@users.noreply.github.com>
2022-11-12 14:34:43 +0630

    addSlide

--- app.js/home.js
+++ app.js/home.js
@@ -1,1 +1,5 @@
 console.log("hello world");
+let headBar = document.querySelector(".main-menu");
+window.addEventListener("scroll",function(){
+    headBar.classList.toggle("main-menu-slide" , window.scrollY > 0)
+})
